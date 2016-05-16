feff746855bf51d0062fad5daa8f7a168d90ddbe
walter <dqyi11@gmail.com>
2016-05-15 23:19:42 -0600

    add time delay

--- sphero_driver/scripts/test_two_sphero_driver.py
+++ sphero_driver/scripts/test_two_sphero_driver.py
@@ -7,8 +7,8 @@ import sys
 
 SPHERO1_NAME = "Sphero-RBY"
 SPHERO1_ADDR = "68:86:E7:07:85:E9"
-SPHERO2_NAME = "Sphero-ROB"
-SPHERO2_ADDR = "68:86:E7:02:8D:CF"
+SPHERO2_NAME = "Sphero-OGG"
+SPHERO2_ADDR = "68:86:E7:02:8B:A0"
 
 sphero1 = sphero_driver.Sphero(SPHERO1_NAME, SPHERO1_ADDR)
 sphero2 = sphero_driver.Sphero(SPHERO2_NAME, SPHERO2_ADDR)
@@ -22,41 +22,51 @@ sphero2.start()
 time.sleep(2)
 print "disable stabilization"
 sphero1.set_stablization(0, False)
+time.sleep(2)
 sphero2.set_stablization(0, False)
 time.sleep(3)
 print "set color to RED"
 sphero1.set_rgb_led(255,0,0,0,False)
+time.sleep(2)
 sphero2.set_rgb_led(255,0,0,0,False)
 time.sleep(1)
 print "set color to GREEN"
 sphero1.set_rgb_led(0,255,0,0,False)
+time.sleep(2)
 sphero2.set_rgb_led(0,255,0,0,False)
-time.sleep(1)
+time.sleep(2)
 print "set color to BLUE"
 sphero1.set_rgb_led(0,0,255,0,False)
+time.sleep(2)
 sphero2.set_rgb_led(0,0,255,0,False)
 time.sleep(3)
 print "set back led"
 sphero1.set_rgb_led(255,255,255,0,False)
+time.sleep(2)
 sphero2.set_rgb_led(255,255,255,0,False)
+time.sleep(2)
 sphero1.set_back_led(255,False)
+time.sleep(2)
 sphero2.set_back_led(255,False)
-time.sleep(3)
 time.sleep(3)
 print "enable stablization"
 sphero1.set_stablization(1, False)
+time.sleep(2)
 sphero2.set_stablization(1, False)
-time.sleep(5)
+time.sleep(2)
 print "set aiming"
 sphero1.roll(0, 90, 0, False)
+time.sleep(2)
 sphero1.set_heading(90, False)
+time.sleep(2)
 sphero2.roll(0, 90, 0, False)
+time.sleep(2)
 sphero2.set_heading(90, False)
 time.sleep(3)
-time.sleep(15)
 #sphero.join()
 print "disconnet"
 sphero1.disconnect()
+time.sleep(3)
 sphero2.disconnect()
 sys.exit(1)
 
