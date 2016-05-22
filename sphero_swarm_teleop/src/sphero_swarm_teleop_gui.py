6eb74777184bb4071e85e9cc1b940e337346b871
walter <daqing.yi@byu.edu>
2016-05-22 17:38:16 -0600

    fix bug:

--- sphero_swarm_teleop/src/sphero_swarm_teleop_gui.py
+++ sphero_swarm_teleop/src/sphero_swarm_teleop_gui.py
@@ -2,30 +2,23 @@
 
 import sys, rospy, math
 from PyQt4 import QtGui, QtCore
-from geometry_msgs.msg import Twist, Pose2D
-from std_msgs.msg import ColorRGBA, Float32, Bool
-from apriltags_intrude_detector.srv import apriltags_intrude
-from apriltags_intrude_detector.srv import apriltags_info
 
-class SpheroIntrudeForm(QtGui.QWidget):
+from sphero_swarm_node.msg import SpheroTwist
+
+class SpheroSwarmTeleopForm(QtGui.QWidget):
     
     def __init__(self):
         super(QtGui.QWidget, self).__init__()
         self.resize(600, 480) 
+        self.sphero_dict = {}
         self.initUI()
 
-        rospy.init_node('sphero_intrude', anonymous=True)
-        self.cmdVelPub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-        self.cmdVelSub = rospy.Subscriber("cmd_vel", Twist, self.cmdVelCallback) 
+        rospy.init_node('sphero_swarm_teleop_gui', anonymous=True)
+        self.cmdVelPub = rospy.Publisher('cmd_vel', SpheroTwist, queue_size=1)
+        self.cmdVelSub = rospy.Subscriber("cmd_vel", SpheroTwist, self.cmdVelCallback) 
 
-        self.trackposSub = rospy.Subscriber("tracked_pos", Pose2D, self.trackposCallback) 
        
-    def initUI(self):
-
-        self.stateLabel = QtGui.QLabel("Position")
-        self.stateTextbox = QtGui.QTextEdit()
-        self.stateTextbox.setReadOnly(True)
-        self.connect(self, QtCore.SIGNAL("sendPosIDText(PyQt_PyObject)"), self.updateStateTextbot)     
+    def initUI(self):   
         
         key_instruct_label = """
 	Control Your Sphero!
@@ -41,72 +34,74 @@ class SpheroIntrudeForm(QtGui.QWidget):
         self.cmdVelTextbox.setReadOnly(True)  
         self.connect(self, QtCore.SIGNAL("sendCmdVelText(PyQt_PyObject)"), self.updateCmdVelTextbox)
 
-        self.aprilTagsInfoLabel = QtGui.QLabel("april tags info")
-        self.aprilTagsInfoBtn = QtGui.QPushButton("Query")
-        self.aprilTagsInfoBtn.clicked.connect(self.queryAprilTagsInfo)
-        self.aprilTagsTextbox = QtGui.QTextEdit()
-        self.aprilTagsTextbox.setReadOnly(True)
-        self.connect(self, QtCore.SIGNAL("sendTagInfoText(PyQt_PyObject)"), self.updateAprilTagsTextbot)
+        self.spheroLabel = QtGui.QLabel("Spheros:")
+        self.spheroListWidget = QtGui.QListWidget()
+        self.refreshBtn = QtGui.QPushButton("Refresh")
+        self.refreshBtn.clicked.connect(self.refreshDevices)
+        btnGridLayout = QtGui.QGridLayout()
+        btnGridLayout.addWidget(self.refreshBtn, 0, 4)
 
-        self.layout =  QtGui.QVBoxLayout()
-        self.layout.addWidget(self.stateLabel)
-        self.layout.addWidget(self.stateTextbox)
-        self.layout.addWidget(self.keyInstructLabel)
-        self.layout.addWidget(self.cmdVelLabel)
-        self.layout.addWidget(self.cmdVelTextbox)
-        hlayout = QtGui.QHBoxLayout()
-        hlayout.addWidget(self.aprilTagsInfoLabel)
-        hlayout.addWidget(self.aprilTagsInfoBtn)
-        self.layout.addLayout(hlayout)
-        self.layout.addWidget(self.aprilTagsTextbox)
-        self.setLayout(self.layout)
+        layout =  QtGui.QVBoxLayout()
+        layout.addWidget(self.keyInstructLabel)
+        layout.addWidget(self.cmdVelLabel)
+        layout.addWidget(self.cmdVelTextbox)
+        layout.addWidget(self.spheroLabel)
+        layout.addWidget(self.spheroListWidget)
+        layout.addLayout(btnGridLayout)
+        self.setLayout(layout)
 
-        self.setWindowTitle("Sphero Intrude")
+        self.setWindowTitle("Sphero Swarm Teleop")
         self.show()
 
     def keyPressEvent(self, e): 
-        twist = None       
+        twist = None    
+        selected_items = self.spheroListWidget.selectedItems()
+        if len(selected_items) == 0:
+            return
+           
         if e.key() == QtCore.Qt.Key_U:
-            twist = Twist()
+            twist = SpheroTwist() 
             twist.linear.x = -20; twist.linear.y = 20; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
         elif e.key() == QtCore.Qt.Key_I:
-            twist = Twist()  
+            twist = SpheroTwist()  
             twist.linear.x = 0; twist.linear.y = 20; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0     
         elif e.key() == QtCore.Qt.Key_O:
-            twist = Twist()
+            twist = SpheroTwist()
             twist.linear.x = 20; twist.linear.y = 20; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
         elif e.key() == QtCore.Qt.Key_J:
-            twist = Twist()
+            twist = SpheroTwist()
             twist.linear.x = -20; twist.linear.y = 0; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
         elif e.key() == QtCore.Qt.Key_K:
-            twist = Twist()
+            twist = SpheroTwist()
             twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
         elif e.key() == QtCore.Qt.Key_L:
-            twist = Twist()
+            twist = SpheroTwist()
             twist.linear.x = 20; twist.linear.y = 0; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
         elif e.key() == QtCore.Qt.Key_M:
-            twist = Twist()
+            twist = SpheroTwist()
             twist.linear.x = -20; twist.linear.y = -20; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
         elif e.key() == QtCore.Qt.Key_Comma:
-            twist = Twist()
+            twist = SpheroTwist()
             twist.linear.x = 0; twist.linear.y = -20; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
         elif e.key() == QtCore.Qt.Key_Period:
-            twist = Twist()
+            twist = SpheroTwist()
             twist.linear.x = 20; twist.linear.y = -20; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0 
+
         if twist != None:
+            twist.name = selected_items[0]
             self.cmdVelPub.publish(twist)
 
     def cmdVelCallback(self, msg):
-        cmd_vel_text = "x=" + str(msg.linear.x) + " y=" + str(msg.linear.y)
+        cmd_vel_text = "(" + str(msg.name) + "),x=" + str(msg.linear.x) + " y=" + str(msg.linear.y)
         self.emit(QtCore.SIGNAL("sendCmdVelText(PyQt_PyObject)"), cmd_vel_text) 
 
     def updateCmdVelTextbox(self, value):
@@ -115,58 +110,22 @@ class SpheroIntrudeForm(QtGui.QWidget):
         self.cmdVelTextbox.append(str(value))
         self.cmdVelTextbox.update()
 
-    def trackposCallback(self, msg):
-        rospy.wait_for_service("apriltags_intrude")
-        try:
-            intrude_query = rospy.ServiceProxy("apriltags_intrude", apriltags_intrude)
-            resp = intrude_query(int(msg.x), int(msg.y))
-            pos_id_text = "["+str(int(msg.x))+"," +str(int(msg.y))+"]" + "(" + str(resp.id) + ")"
-            self.emit(QtCore.SIGNAL("sendPosIDText(PyQt_PyObject)"), pos_id_text)
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
 
-    def updateStateTextbot(self, value):
-        self.stateTextbox.moveCursor(QtGui.QTextCursor.End)
-        self.stateTextbox.ensureCursorVisible()
-        self.stateTextbox.append(str(value))
-        self.stateTextbox.update()
+    def refreshDevices(self):
+        self.spheroListWidget.clear()
+        self.sphero_dict = rospy.get_param('/sphero_swarm/connected')
 
-    def queryAprilTagsInfo(self):
-        #print "clicked"
-        rospy.wait_for_service("apriltags_info")
-        try:
-            info_query = rospy.ServiceProxy("apriltags_info", apriltags_info)
-            resp = info_query()
-               
-            #print str(resp)
+        print(self.sphero_dict)
 
-            info_text = "" 
-            for i in range(len(resp.polygons)):
-                poly = resp.polygons[i]
-                t_id = resp.ids[i]
+        for name in self.sphero_dict:
+            self.spheroListWidget.addItem(name)
+        self.update()
 
-                #print(str(poly))
-                #print(str(t_id))
-                info_text += "["+str(t_id)+"] "
-                for p in poly.points:
-                    info_text += "(" + str(int(p.x)) + "," + str(int(p.y)) + ")"
-                info_text += "\n" 
-
-            self.emit(QtCore.SIGNAL("sendTagInfoText(PyQt_PyObject)"), info_text)
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
-        
-    def updateAprilTagsTextbot(self, value):
-        self.aprilTagsTextbox.clear()
-        self.aprilTagsTextbox.moveCursor(QtGui.QTextCursor.End)
-        self.aprilTagsTextbox.ensureCursorVisible()
-        self.aprilTagsTextbox.append(str(value))
-        self.aprilTagsTextbox.update()        
-
+ 
 if __name__ == '__main__':
 
     app = QtGui.QApplication(sys.argv)
-    w = SpheroIntrudeForm()
+    w = SpheroSwarmTeleopForm()
     w.show()
     sys.exit(app.exec_())
   
